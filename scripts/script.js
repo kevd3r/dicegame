431b3f219e003327883f2598ebc7ba06d933197e
kevd3r <kevin.derot@gmail.com>
2022-03-19 23:41:15 +0100

    add alternate player functions

---
+++ scripts/script.js
@@ -0,0 +1,55 @@
+// random who's beginning the game
+function wichOneBegins(){
+  return Math.ceil(Math.random()*2);
+}
+console.log(wichOneBegins());
+
+// Dice rolling function
+function rollDice(){
+  return Math.ceil(Math.random()*6);
+}
+console.log(rollDice());
+
+
+// Create player's objects
+function Player (name, turn, global,isActive){
+  this.name=name;
+  this.turn=turn;
+  this.global=global;
+  this.isActive=isActive;
+};
+const player1= new Player('Joueur1',1,0,false);
+const player2= new Player('Joueur2',2,0,false);
+console.log(player1, player2);
+
+let newgame = document.getElementById('new-game');
+let p1 = document.getElementById('player1');
+let p2 = document.getElementById('player2');
+
+/*---------- starting a new game -------------*/
+function newGame(){
+  player1.global = 0;
+  player2.global = 0;
+  let playerBegins = wichOneBegins();
+  if (playerBegins ===1){
+    player1isActive();
+  }else{
+    player2isActive();
+  }
+}
+
+newgame.addEventListener('click', newGame);
+/* ------ changing player functions --------------*/
+function player1isActive (){
+  player1.isActive = "true";
+  p1.classList.add('is-active');
+  p2.classList.remove('is-active');
+  player2.isActive = "false";
+}
+
+function player2isActive (){
+  p2.classList.add('is-active');
+  p1.classList.remove('is-active');
+  player2.isActive = "true";
+  player1.isActive = "false";
+}
